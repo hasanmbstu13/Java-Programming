ac80298c69dd73745e0c5a4266f5a3ba8b30ab2d
hasanmbstu13 <hasanmbstu13@gmail.com>
2014-10-23 07:25:26 +0600

    Make a basic calculator --Use double type data type --nextDouble() is used for double data type

--- GettingUserInput/src/GetUserInput.java
+++ GettingUserInput/src/GetUserInput.java
@@ -8,6 +8,14 @@ public class GetUserInput {
 		System.out.println("May I know your name please?\n");
 		String name = myInput.nextLine();
 		System.out.println("Your name is " + name);
+		
+		/*
+		 * In Java programming Scanner is used to get input from user
+		 * Basic Syntax 
+		 * import java.util.Scanner
+		 * Scanner myInput(var name) = new Scanner(System.in);
+		 * myInput.nextLine() //to get input from user
+		 */
 	}
 
 }
